5810d70e27f3dd41ca6a504a9f80a3e218a36707
LuShanli <15607732513@163.com>
2020-06-08 16:13:28 +0800

    js实现斗地主

---
+++ doudizhu/game/ResourceData.js
@@ -0,0 +1,83 @@
+
+
+var ResourceData= {
+    Images:{
+        //path：文件相对路径，picNum:当前图片中宽和高包含的小图片数，cellSize:小图片的宽和高，data：存放加载的图片数据。
+        //goddess_new:{path:"images/goddess_new.png", picNum:{WNum:4,HNum:4},cellSize:{width:32,height:48}, data:null}
+        1:{path:"game/img/1.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:18}
+        ,2:{path:"game/img/2.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:17}
+        ,3:{path:"game/img/3.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:3}
+        ,4:{path:"game/img/4.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:4}
+        ,5:{path:"game/img/5.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:5}
+        ,6:{path:"game/img/6.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:6}
+        ,7:{path:"game/img/7.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:7}
+        ,8:{path:"game/img/8.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:8}
+        ,9:{path:"game/img/9.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:9}
+        ,10:{path:"game/img/10.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:10}
+        ,11:{path:"game/img/11.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:11}
+        ,12:{path:"game/img/12.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:12}
+        ,13:{path:"game/img/13.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:13}
+        ,14:{path:"game/img/14.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:14}
+        ,15:{path:"game/img/15.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:16}
+        ,16:{path:"game/img/16.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:3}
+        ,17:{path:"game/img/17.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:4}
+        ,18:{path:"game/img/18.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:5}
+        ,19:{path:"game/img/19.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:6}
+        ,20:{path:"game/img/20.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:7}
+        ,21:{path:"game/img/21.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:8}
+        ,22:{path:"game/img/22.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:9}
+        ,23:{path:"game/img/23.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:10}
+        ,24:{path:"game/img/24.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:11}
+        ,25:{path:"game/img/25.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:12}
+        ,26:{path:"game/img/26.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:13}
+        ,27:{path:"game/img/27.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:14}
+        ,28:{path:"game/img/28.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:16}
+        ,29:{path:"game/img/29.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:3}
+        ,30:{path:"game/img/30.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:4}
+        ,31:{path:"game/img/31.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:5}
+        ,32:{path:"game/img/32.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:6}
+        ,33:{path:"game/img/33.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:7}
+        ,34:{path:"game/img/34.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:8}
+        ,35:{path:"game/img/35.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:9}
+        ,36:{path:"game/img/36.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:10}
+        ,37:{path:"game/img/37.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:11}
+        ,38:{path:"game/img/38.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:12}
+        ,39:{path:"game/img/39.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:13}
+        ,40:{path:"game/img/40.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:14}
+        ,41:{path:"game/img/41.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:16}
+        ,42:{path:"game/img/42.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:3}
+        ,43:{path:"game/img/43.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:4}
+        ,44:{path:"game/img/44.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:5}
+        ,45:{path:"game/img/45.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:6}
+        ,46:{path:"game/img/46.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:7}
+        ,47:{path:"game/img/47.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:8}
+        ,48:{path:"game/img/48.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:9}
+        ,49:{path:"game/img/49.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:10}
+        ,50:{path:"game/img/50.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:11}
+        ,51:{path:"game/img/51.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:12}
+        ,52:{path:"game/img/52.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:13}
+        ,53:{path:"game/img/53.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:14}
+        ,54:{path:"game/img/54.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:16}
+        ,BeiMian:{path:"game/img/BeiMian.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null}
+        ,bg1:{path:"game/img/bg1.png", picNum:{WNum:1,HNum:1},cellSize:{width:640,height:320}, data:null}
+        ,dz:{path:"game/img/dz.png", picNum:{WNum:1,HNum:1},cellSize:{width:30,height:30}, data:null}
+        ,nm:{path:"game/img/nm.png", picNum:{WNum:1,HNum:1},cellSize:{width:30,height:30}, data:null}
+        ,kaishi:{path:"game/img/kaishi.png", picNum:{WNum:1,HNum:1},cellSize:{width:130,height:50}, data:null}
+        ,buchu:{path:"game/img/buchu.png", picNum:{WNum:1,HNum:1},cellSize:{width:100,height:50}, data:null}
+        ,tishi:{path:"game/img/tishi.png", picNum:{WNum:1,HNum:1},cellSize:{width:100,height:50}, data:null}
+        ,chupai:{path:"game/img/chupai.png", picNum:{WNum:1,HNum:1},cellSize:{width:100,height:50}, data:null}
+        ,buqiang:{path:"game/img/buqiang.png", picNum:{WNum:1,HNum:1},cellSize:{width:130,height:50}, data:null}
+        ,yf:{path:"game/img/yf.png", picNum:{WNum:1,HNum:1},cellSize:{width:130,height:50}, data:null}
+        ,ef:{path:"game/img/ef.png", picNum:{WNum:1,HNum:1},cellSize:{width:130,height:50}, data:null}
+        ,sf:{path:"game/img/sf.png", picNum:{WNum:1,HNum:1},cellSize:{width:130,height:50}, data:null}
+    },
+    Sound:{
+        //soundName:声音文件名称，path：文件夹相对路径，data：存放加载的声音数据。由于各种浏览器对声音格式的支持不一致，声音文件格式有MP3和OGG两种
+        //sound01:{soundName:"sound01",path:"sound/", data:null,num:17}
+    },
+    Animation:{
+         //ZS1_move:{imageName:"goddess_new",beginPoint:{WNum:0,HNum:0},allPlayNum:4,times:2}
+    }
+
+
+};
