2f13de68d501cb34673a830b13b0d91e5fd0003d
LuShanli <15607732513@163.com>
2020-06-30 11:58:01 +0800

    （实现增加，修改，查询功能）

---
+++ Challenger2/src/com/user/Allusers.java
@@ -0,0 +1,18 @@
+package com.user;
+
+public class Allusers {
+private String id;
+User u;
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public User getU() {
+	return u;
+}
+public void setU(User u) {
+	this.u = u;
+}
+}
